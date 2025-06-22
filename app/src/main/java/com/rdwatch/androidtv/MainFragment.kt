212f37d98957bc69b9ad88dfb2a7c668b5884346
adinunzio10 <adinunzio@alfredoautomation.com>
2025-06-22 01:12:05 -0400

    fix: replace deprecated activity!! calls with requireActivity() for safer fragment handling

--- app/src/main/java/com/rdwatch/androidtv/MainFragment.kt
+++ app/src/main/java/com/rdwatch/androidtv/MainFragment.kt
@@ -1,15 +1,19 @@
 package com.rdwatch.androidtv
 
-import java.util.Collections
-import java.util.Timer
-import java.util.TimerTask
-
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
 import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
@@ -22,22 +26,14 @@ import androidx.leanback.widget.OnItemViewSelectedListener
 import androidx.leanback.widget.Presenter
 import androidx.leanback.widget.Row
 import androidx.leanback.widget.RowPresenter
-import androidx.core.app.ActivityOptionsCompat
-import androidx.core.content.ContextCompat
-import android.util.DisplayMetrics
-import android.util.Log
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import java.util.Collections
+import java.util.Timer
+import java.util.TimerTask
 
-/**
- * Loads a grid of cards with movies to browse.
- */
+/** Loads a grid of cards with movies to browse. */
 class MainFragment : BrowseSupportFragment() {
 
     private val mHandler = Handler(Looper.myLooper()!!)
@@ -68,11 +64,11 @@ class MainFragment : BrowseSupportFragment() {
 
     private fun prepareBackgroundManager() {
 
-        mBackgroundManager = BackgroundManager.getInstance(activity)
-        mBackgroundManager.attach(activity!!.window)
-        mDefaultBackground = ContextCompat.getDrawable(activity!!, R.drawable.default_background)
+        mBackgroundManager = BackgroundManager.getInstance(requireActivity())
+        mBackgroundManager.attach(requireActivity().window)
+        mDefaultBackground = ContextCompat.getDrawable(requireActivity(), R.drawable.default_background)
         mMetrics = DisplayMetrics()
-        activity!!.windowManager.defaultDisplay.getMetrics(mMetrics)
+        requireActivity().windowManager.defaultDisplay.getMetrics(mMetrics)
     }
 
     private fun setupUIElements() {
@@ -82,9 +78,9 @@ class MainFragment : BrowseSupportFragment() {
         isHeadersTransitionOnBackEnabled = true
 
         // set fastLane (or headers) background color
-        brandColor = ContextCompat.getColor(activity!!, R.color.fastlane_background)
+        brandColor = ContextCompat.getColor(requireActivity(), R.color.fastlane_background)
         // set search icon color
-        searchAffordanceColor = ContextCompat.getColor(activity!!, R.color.search_opaque)
+        searchAffordanceColor = ContextCompat.getColor(requireActivity(), R.color.search_opaque)
     }
 
     private fun loadRows() {
@@ -119,8 +115,7 @@ class MainFragment : BrowseSupportFragment() {
 
     private fun setupEventListeners() {
         setOnSearchClickedListener {
-            Toast.makeText(activity!!, "Implement your own in-app search", Toast.LENGTH_LONG)
-                .show()
+            Toast.makeText(requireActivity(), "Implement your own in-app search", Toast.LENGTH_LONG).show()
         }
 
         onItemViewClickedListener = ItemViewClickedListener()
@@ -129,30 +124,31 @@ class MainFragment : BrowseSupportFragment() {
 
     private inner class ItemViewClickedListener : OnItemViewClickedListener {
         override fun onItemClicked(
-            itemViewHolder: Presenter.ViewHolder,
-            item: Any,
-            rowViewHolder: RowPresenter.ViewHolder,
-            row: Row
+                itemViewHolder: Presenter.ViewHolder,
+                item: Any,
+                rowViewHolder: RowPresenter.ViewHolder,
+                row: Row
         ) {
 
             if (item is Movie) {
                 Log.d(TAG, "Item: " + item.toString())
-                val intent = Intent(activity!!, DetailsActivity::class.java)
+                val intent = Intent(requireActivity(), DetailsActivity::class.java)
                 intent.putExtra(DetailsActivity.MOVIE, item)
 
-                val bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                    activity!!,
-                    (itemViewHolder.view as ImageCardView).mainImageView,
-                    DetailsActivity.SHARED_ELEMENT_NAME
-                )
-                    .toBundle()
+                val bundle =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                        requireActivity(),
+                                        (itemViewHolder.view as ImageCardView).mainImageView,
+                                        DetailsActivity.SHARED_ELEMENT_NAME
+                                )
+                                .toBundle()
                 startActivity(intent, bundle)
             } else if (item is String) {
                 if (item.contains(getString(R.string.error_fragment))) {
-                    val intent = Intent(activity!!, BrowseErrorActivity::class.java)
+                    val intent = Intent(requireActivity(), BrowseErrorActivity::class.java)
                     startActivity(intent)
                 } else {
-                    Toast.makeText(activity!!, item, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireActivity(), item, Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -160,8 +156,10 @@ class MainFragment : BrowseSupportFragment() {
 
     private inner class ItemViewSelectedListener : OnItemViewSelectedListener {
         override fun onItemSelected(
-            itemViewHolder: Presenter.ViewHolder?, item: Any?,
-            rowViewHolder: RowPresenter.ViewHolder, row: Row
+                itemViewHolder: Presenter.ViewHolder?,
+                item: Any?,
+                rowViewHolder: RowPresenter.ViewHolder,
+                row: Row
         ) {
             if (item is Movie) {
                 mBackgroundUri = item.backgroundImageUrl
@@ -173,19 +171,20 @@ class MainFragment : BrowseSupportFragment() {
     private fun updateBackground(uri: String?) {
         val width = mMetrics.widthPixels
         val height = mMetrics.heightPixels
-        Glide.with(activity!!)
-            .load(uri)
-            .centerCrop()
-            .error(mDefaultBackground)
-            .into<SimpleTarget<Drawable>>(
-                object : SimpleTarget<Drawable>(width, height) {
-                    override fun onResourceReady(
-                        drawable: Drawable,
-                        transition: Transition<in Drawable>?
-                    ) {
-                        mBackgroundManager.drawable = drawable
-                    }
-                })
+        Glide.with(requireActivity())
+                .load(uri)
+                .centerCrop()
+                .error(mDefaultBackground)
+                .into<SimpleTarget<Drawable>>(
+                        object : SimpleTarget<Drawable>(width, height) {
+                            override fun onResourceReady(
+                                    drawable: Drawable,
+                                    transition: Transition<in Drawable>?
+                            ) {
+                                mBackgroundManager.drawable = drawable
+                            }
+                        }
+                )
         mBackgroundTimer?.cancel()
     }
 
@@ -208,7 +207,7 @@ class MainFragment : BrowseSupportFragment() {
             view.layoutParams = ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT)
             view.isFocusable = true
             view.isFocusableInTouchMode = true
-            view.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.default_background))
+            view.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.default_background))
             view.setTextColor(Color.WHITE)
             view.gravity = Gravity.CENTER
             return Presenter.ViewHolder(view)
